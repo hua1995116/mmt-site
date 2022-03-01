938d68a56a45a05548ef63d88edac5edfd3ecd61
hua1995116 <461249104@qq.com>
2022-03-01 23:48:03 +0800

    feat: baidu analysis

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
       <Html lang={AppConfig.locale}>
         <Head>
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-2PMZYB5X0C"></script>
-          <script dangerouslySetInnerHTML={{
+        <script dangerouslySetInnerHTML={{
             __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
@@ -17,6 +17,19 @@ class MyDocument extends Document {
             gtag('config', 'G-2PMZYB5X0C');
             `
         }}>
+        </script>
+        <script dangerouslySetInnerHTML={{
+            __html: `
+          var _hmt = _hmt || [];
+          (function() {
+            var hm = document.createElement("script");
+            hm.src = "https://hm.baidu.com/hm.js?6204d0a89e4eeb98a5852676b8f9b59c";
+            var s = document.getElementsByTagName("script")[0]; 
+            s.parentNode.insertBefore(hm, s);
+          })();
+            `
+          }}>
+            
         </script>
         </Head>
 
